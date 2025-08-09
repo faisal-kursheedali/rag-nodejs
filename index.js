2287f84b1bdc3e8e20ce35572fb7a973b243358f
FAISAL <85350108+faisal-kursheedali@users.noreply.github.com>
2025-08-09 12:46:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { RAG } from "./rag.js";
 import chalk from "chalk";
 
 async function main() {
-  const dataPath = path.join(process.cwd(), "data2.json");
+  const dataPath = path.join(process.cwd(), "data.json");
   // Huge data sample -> but we get irrelevent response bcoz data set is not correct
-  // const dataPath = path.join(process.cwd(), "data.json");
+  // const dataPath = path.join(process.cwd(), "data2.json");
   const data = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
 
   const rag = new RAG(data);
